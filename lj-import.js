1e05457cd119e54d9893ac238d1f708edf806073
Azubuike Motanya <acmotanya@gmail.com>
2019-01-24 22:59:32 -0500

    adding and changing a few things.

--- lj-import.js
+++ lj-import.js
@@ -21,10 +21,10 @@ server.listen(port, hostname, () => {
 
 
 
-function getProducts() {
+function getProducts2() {
   sql.connect(dbconfig).then(pool =>  {
     return pool.request()
-    .query("SELECT dbo.cca_item_descriptions.vitemnumber AS vItemNumber, dbo.cca_item_descriptions.vlocation AS vLocation, dbo.cca_item_descriptions.vdescription AS vDescription, dbo.cca_item_descriptions.vshortdesc AS vShortDesc, dbo.cca_item_descriptions.vlook AS vLook, dbo.cca_item_descriptions.vspecificcolor AS vSpecificColor, dbo.cca_item_descriptions.vgencolor AS vGenColor, dbo.cca_item_descriptions.vgenmaterial AS vGenMaterial, dbo.cca_item_descriptions.vgenitemtype AS vGenItemType, dbo.cca_item_descriptions.vshape AS vShape, dbo.cca_item_descriptions.vsizetype AS vSizetype, dbo.cca_item_descriptions.vmetalcolor AS vMetalColor, dbo.cca_item_descriptions.vmetaltype AS vMetalType, dbo.cca_item_descriptions.vdimensions AS vDimensions, dbo.cca_item_descriptions.vpccounts AS vPcCounts, dbo.cca_item_descriptions.vkeywords AS vKeywords, dbo.cca_item_descriptions.vonsale AS vOnSale, dbo.cca_item_descriptions.vfeatureditem  AS vFeaturedItem, dbo.cca_item_descriptions.vsorting AS vSorting, dbo.cca_item_descriptions.vaggregation AS vAggregation,dbo.cca_item_descriptions.vEvents AS vEvents, dbo.cca_item_descriptions.vDetailDesc AS vDetailDesc, dbo.cca_item_descriptions.vFeatureDesc AS vFeatureDesc, dbo.cca_item_descriptions.vID AS vID, dbo.cca_item_descriptions.vLastUpdated AS vLastUpdated, dbo.cca_item_descriptions.vLastUpdated AS vLastUpdated, dbo.cca_item_descriptions.vMaterialDesc  AS vMaterialDesc, dbo.cca_item_descriptions.vShowOnSite AS vAShowOnSite, FROM dbo.cca_item_descriptions WHERE dbo.cca_item_descriptions.vshowonsite = 'Y' AND dbo.cca_item_descriptions.vlocation = '800'");
+    .query("SELECT dbo.cca_item_descriptions.vitemnumber AS vItemNumber, dbo.cca_item_descriptions.vlocation AS vLocation, dbo.cca_item_descriptions.vdescription AS vDescription, dbo.cca_item_descriptions.vshortdesc AS vShortDesc, dbo.cca_item_descriptions.vlook AS vLook, dbo.cca_item_descriptions.vspecificcolor AS vSpecificColor, dbo.cca_item_descriptions.vgencolor AS vGenColor, dbo.cca_item_descriptions.vgenmaterial AS vGenMaterial, dbo.cca_item_descriptions.vgenitemtype AS vGenItemType, dbo.cca_item_descriptions.vshape AS vShape, dbo.cca_item_descriptions.vsizetype AS vSizetype, dbo.cca_item_descriptions.vmetalcolor AS vMetalColor, dbo.cca_item_descriptions.vmetaltype AS vMetalType, dbo.cca_item_descriptions.vdimensions AS vDimensions, dbo.cca_item_descriptions.vpccounts AS vPcCounts, dbo.cca_item_descriptions.vkeywords AS vKeywords, dbo.cca_item_descriptions.vonsale AS vOnSale, dbo.cca_item_descriptions.vfeatureditem  AS vFeaturedItem, dbo.cca_item_descriptions.vsorting AS vSorting, dbo.cca_item_descriptions.vaggregation AS vAggregation,dbo.cca_item_descriptions.vEvents AS vEvents, dbo.cca_item_descriptions.vDetailDesc AS vDetailDesc, dbo.cca_item_descriptions.vFeatureDesc AS vFeatureDesc, dbo.cca_item_descriptions.vID AS vID, dbo.cca_item_descriptions.vLastUpdated AS vLastUpdated, dbo.cca_item_descriptions.vLastUpdated AS vLastUpdated, dbo.cca_item_descriptions.vMaterialDesc  AS vMaterialDesc, dbo.cca_item_descriptions.vShowOnSite AS vShowOnSite, FROM dbo.cca_item_descriptions WHERE dbo.cca_item_descriptions.vshowonsite = 'Y' AND dbo.cca_item_descriptions.vlocation = '800'");
   }).then(result => {
       items = JSON.stringify(result.recordset);
 			items = JSON.parse(items.replace(/"\s+|\s+"/g,'"'));
@@ -43,6 +43,23 @@ function getProducts() {
   sql.on('error', err => {
     // ... error handler
     console.log(err);
+  });
+}
+
+
+function getProducts() {
+	sql.connect(dbconfig).then(() =>  {
+    var request = new sql.Request();
+    request.query("SELECT dbo.cca_item_descriptions.vitemnumber AS vItemNumber, dbo.cca_item_descriptions.vlocation AS vLocation, dbo.cca_item_descriptions.vdescription AS vDescription, dbo.cca_item_descriptions.vshortdesc AS vShortDesc, dbo.cca_item_descriptions.vlook AS vLook, dbo.cca_item_descriptions.vspecificcolor AS vSpecificColor, dbo.cca_item_descriptions.vgencolor AS vGenColor, dbo.cca_item_descriptions.vgenmaterial AS vGenMaterial, dbo.cca_item_descriptions.vgenitemtype AS vGenItemType, dbo.cca_item_descriptions.vshape AS vShape, dbo.cca_item_descriptions.vsizetype AS vSizetype, dbo.cca_item_descriptions.vmetalcolor AS vMetalColor, dbo.cca_item_descriptions.vmetaltype AS vMetalType, dbo.cca_item_descriptions.vdimensions AS vDimensions, dbo.cca_item_descriptions.vpccounts AS vPcCounts, dbo.cca_item_descriptions.vkeywords AS vKeywords, dbo.cca_item_descriptions.vonsale AS vOnSale, dbo.cca_item_descriptions.vfeatureditem  AS vFeaturedItem, dbo.cca_item_descriptions.vsorting AS vSorting, dbo.cca_item_descriptions.vaggregation AS vAggregation,dbo.cca_item_descriptions.vEvents AS vEvents, dbo.cca_item_descriptions.vDetailDesc AS vDetailDesc, dbo.cca_item_descriptions.vFeatureDesc AS vFeatureDesc, dbo.cca_item_descriptions.vID AS vID, dbo.cca_item_descriptions.vLastUpdated AS vLastUpdated, dbo.cca_item_descriptions.vLastUpdated AS vLastUpdated, dbo.cca_item_descriptions.vMaterialDesc  AS vMaterialDesc, dbo.cca_item_descriptions.vShowOnSite AS vShowOnSite, FROM dbo.cca_item_descriptions WHERE dbo.cca_item_descriptions.vshowonsite = 'Y' AND dbo.cca_item_descriptions.vlocation = '800'", (err, result) => {
+      items = JSON.stringify(result.recordset);
+			items = JSON.parse(items.replace(/"\s+|\s+"/g,'"'));
+
+      fs.writeFile('items800.js', JSON.stringify(items), 'utf8', (error) => {
+        if (error)
+          console.log(error);
+        console.log("Query Complete");
+      });
+    });
   });
 }
 
